9e1f308f2933f055b87381aef758f3e3bf075eae
Peter-Kibet <peter.kibet@hotmail.com>
2024-10-29 04:08:27 +0300

    Refactor webpack.mix.js to simplify configuration and remove unused Sass processing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,11 @@
 const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 mix.js("resources/src/main.js", "public")
     .js("resources/src/login.js", "public")
-    .vue()
-    .sass("resources/sass/app.scss", "public/css", {
-        implementation: require("sass"),
-    });
+    .vue();
 
 mix.webpackConfig({
     output: {
@@ -32,27 +18,6 @@ mix.webpackConfig({
             cleanOnceBeforeBuildPatterns: ["./js/*"],
         }),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "sass-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass"),
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-});
-
-// Disable processing URLs in CSS/Sass files
-mix.options({
-    processCssUrls: false,
 });
 
 // If you're in development, you might want source maps
