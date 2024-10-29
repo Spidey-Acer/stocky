cebad33345cfd67023bf806f92428c64359afb0d
Peter-Kibet <peter.kibet@hotmail.com>
2024-10-29 04:08:41 +0300

    Add documentation comments to webpack.mix.js for clarity

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,14 @@
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
 const mix = require("laravel-mix");
 
 const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
